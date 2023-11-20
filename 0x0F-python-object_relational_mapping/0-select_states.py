6aa08cb049ed96f42f4f1f2eac396d29473f877c
poquitodev <122402632+poquitodev@users.noreply.github.com>
2023-11-20 01:59:35 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,47 +1,37 @@
 #!/usr/bin/python3
-import MySQLdb
+"""lists all states from mySQL database"""
 import sys
+import MySQLdb
 
-def list_states(username, password, database_name):
+def list_states (username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+    Ags:
+        username: mysql username
+        password: mysql password
+        database: mysql database
+    """
     # Connect to the MySQL server
-    try:
-        connection = MySQLdb.connect(
-            host="localhost",
-            user=username,
-            passwd=password,
-            db=database_name,
-            port=3306
-        )
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")
-        sys.exit(1)
+    db = MySQLdb.connect(host='localhost',port=3306,user=username,passwd=password,db=database)
+    cursor = db.cursor()
 
-    # Create a cursor object to interact with the database
-    cursor = connection.cursor()
+    # Execute the SQL query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Execute the query to retrieve states sorted by id
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cursor.execute(query)
+    # Fetch all the rows from the query result
+    rows = cursor.fetchall()
 
-    # Fetch all the results
-    states = cursor.fetchall()
+    # Print the results
+    for row in rows:
+        print(row)
 
-    # Display the results
-    for state in states:
-        print(state)
+    # Close the database connection
+    db.close()
 
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
+# Example usage
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    # Check if the script is executed with the correct number of arguments
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <username> <password> <database_name>")
-        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # Get MySQL credentials from command line arguments
-    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Call the function to list states
-    list_states(username, password, database_name)
+    list_states(username, password, database)
